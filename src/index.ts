ce15043ccdb9659782237762d230e8ff538cf0c6
AnaLiviaXavier <analiviaxavier333@gmail.com>
2023-07-11 21:25:19 -0300

    feat:server express running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('mudei o texto')
+import express from 'express';
+import {config} from 'dotenv';
+config()
+
+const app = express()
+
+const port = process.env.PORT || 3333
+
+app.get('/teste', (request,response) =>{
+    response.send('Atenção terráquios')
+})
+
+app.listen(port, () => console.log(`Listen on port ${port}!` ))
+
